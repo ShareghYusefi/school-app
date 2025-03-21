573d32a3707992a9fc5cfbd0ee39857dad823e91
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-03-20 19:25:22 -0600

    creating components

---
+++ src/app/custom/custom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom',
+  standalone: false,
+  templateUrl: './custom.component.html',
+  styleUrl: './custom.component.css'
+})
+export class CustomComponent {
+
+}
