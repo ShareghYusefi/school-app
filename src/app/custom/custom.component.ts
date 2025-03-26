42d7d276ce978041760dc7c2e024f9d3fb5e534d
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-03-25 19:22:15 -0600

    style binding

--- src/app/custom/custom.component.ts
+++ src/app/custom/custom.component.ts
@@ -12,4 +12,5 @@ export class CustomComponent {
     'https://www.google.com/imgres?q=google%20logo&imgurl=https%3A%2F%2Fstorage.googleapis.com%2Fgd-prod%2Fimages%2Fa910d418-7123-4bc4-aa3b-ef7e25e74ae6.faa49ab5e1fff880.webp&imgrefurl=https%3A%2F%2Fdesign.google%2Flibrary%2Fevolving-google-identity&docid=cBmV3tv6irC1jM&tbnid=5K1cI-fVUAoOUM&vet=12ahUKEwi_lKvDvaaMAxWdLTQIHU9UC9IQM3oECB0QAA..i&w=1216&h=1216&hcb=2&ved=2ahUKEwi_lKvDvaaMAxWdLTQIHU9UC9IQM3oECB0QAAA';
   alt = 'This is the google logo';
   colSpan = 2;
+  isActive = false;
 }
