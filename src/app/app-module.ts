a75414f7728c98fb7121e80420f3a4f4c0df8a93
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-11-20 18:59:24 -0700

    ngFor directive & @for block

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Custom } from './components/custom/custom';
+import { Course } from './components/course/course';
 
 // You can think of an Angular module like a folder in your project that tells Angular what;s inside it and how the pieces work together.
 @NgModule({
   // components/directives/pipes belong to this module
-  declarations: [App, Custom],
+  declarations: [App, Custom, Course],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
