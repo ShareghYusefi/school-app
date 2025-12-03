60eb10088ff7ac6c9522ba5ce688623a09d34af3
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-12-02 18:48:18 -0700

    routing via routerLink directive

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Courses } from './components/courses/courses';
+import { NotFound } from './components/not-found/not-found';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'courses',
+    component: Courses,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFound },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
